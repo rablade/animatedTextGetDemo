c401dc4e4b1370b2c10a984c5cf07ac4f6281829
strider <strider@Strider>
2023-06-03 08:22:45 -0700

    fix small errors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const r = Math.floor(Math.random() * 256);
 const g = Math.floor(Math.random() * 256);
 const b = Math.floor(Math.random() * 256);
 
-retrun `rgb(${r},${g}${b}`
+return `rgb(${r},${g},${b}`
 }
 
 
